c5d228a9e6c7146de931a4adc5c1a9901cba9683
oniboogie <qwes348@naver.com>
2024-12-15 22:03:50 +0900

    리소스 매니져

---
+++ Manager/ResourceManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+using UnityEngine.AddressableAssets;
+
+public class ResourceManager
+{
+    public async UniTask<T> LoadAsset<T>(string key)
+    {
+        var asset = await Addressables.LoadAssetAsync<T>(key);
+        return asset;
+    }
+
+    public async UniTask<T> LoadAsset<T>(AssetReference reference)
+    {
+        var asset = await Addressables.LoadAssetAsync<T>(reference);
+        return asset;
+    }
+
+    public async UniTask<T> InstantiateAsset<T>(string key, Transform parent = null)
+    {
+        var go = await Addressables.InstantiateAsync(key, parent);
+        return go.GetComponent<T>();
+    }
+
+    public async UniTask<T> InstantiateAsset<T>(AssetReference reference, Transform parent = null)
+    {
+        var go = await Addressables.InstantiateAsync(reference.RuntimeKey, parent);
+        return go.GetComponent<T>();
+    }
+}
